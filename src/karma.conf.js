5153634a84a81acf50a43a39faf3fbd70c96094d
Aliño <raalino@indra.es>
2018-07-24 11:38:15 +0800

    Additional config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,10 +21,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     files: [
-      './node_modules/angular/angular.js',
-      './node_modules/angular-ui-router/release/angular-ui-router.js',
-      './node_modules/angular-mocks/angular-mocks.js',
-      './app/app.js',
+      './app/app.component.spec.ts'
     ],
     //reporters: ['progress', 'kjhtml'],
     reporters: ['progress', 'coverage', 'dots', 'junit'],
