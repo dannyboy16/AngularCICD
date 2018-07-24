2688a4362bb579036b575299cd760ab4b255cfb1
Aliño <raalino@indra.es>
2018-07-24 18:10:18 +0800

    Additional config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,18 +10,19 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      //require('@angular-devkit/build-angular/plugins/karma')
-      require('@angular/cli/plugins/karma')
+      //require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    /*
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+    */
     /*
     files: [
       './app/app.component.spec.ts'
