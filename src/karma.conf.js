255c7afa7d22bb817df3762015f1359f247b7dcd
Aliño <raalino@indra.es>
2018-07-24 13:38:26 +0800

    Additional config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     junitReporter: {
       outputDir: 'karma-results',
       outputFile: 'karma-results.xml'
-    }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
